3ece9e24fce93ca4b86dd0c102a317118db4e48a
Andrea Bocci <andrea.bocci@cern.ch>
2022-06-22 08:33:10 +0200

    Add support for aligned, non-pinned host memory

--- src/cuda/CUDACore/host_unique_ptr.h
+++ src/cuda/CUDACore/host_unique_ptr.h
@@ -10,15 +10,34 @@ namespace cms {
   namespace cuda {
     namespace host {
       namespace impl {
-        // Additional layer of types to distinguish from host::unique_ptr
-        class HostDeleter {
+
+        constexpr size_t default_alignment = 128;
+
+        constexpr size_t pad(size_t size) {
+          return ((size + default_alignment - 1) / default_alignment) * default_alignment;
+        }
+
+        // Additional layer of types to distinguish device::unique_ptr from host::unique_ptr
+        class Deleter {
         public:
-          void operator()(void *ptr) { cms::cuda::free_host(ptr); }
+          Deleter() = default;  // for edm::Wrapper
+          Deleter(bool pinned) : pinned_{pinned} {}
+
+          void operator()(void *ptr) {
+            if (pinned_) {
+              cms::cuda::free_host(ptr);
+            } else {
+              std::free(ptr);
+            }
+          }
+
+        private:
+          bool pinned_ = false;
         };
       }  // namespace impl
 
       template <typename T>
-      using unique_ptr = std::unique_ptr<T, impl::HostDeleter>;
+      using unique_ptr = std::unique_ptr<T, impl::Deleter>;
 
       namespace impl {
         template <typename T>
@@ -36,13 +55,50 @@ namespace cms {
       }  // namespace impl
     }    // namespace host
 
+    // Allocate pageable host memory
+    template <typename T>
+    typename host::impl::make_host_unique_selector<T>::non_array make_host_unique() {
+      static_assert(std::is_trivially_constructible<T>::value,
+                    "Allocating with non-trivial constructor on the pageable host memory is not supported");
+      void *mem = std::aligned_alloc(host::impl::default_alignment, host::impl::pad(sizeof(T)));
+      return typename host::impl::make_host_unique_selector<T>::non_array{reinterpret_cast<T *>(mem),
+                                                                          host::impl::Deleter{false}};
+    }
+
+    template <typename T>
+    typename host::impl::make_host_unique_selector<T>::unbounded_array make_host_unique(size_t n) {
+      using element_type = typename std::remove_extent<T>::type;
+      static_assert(std::is_trivially_constructible<element_type>::value,
+                    "Allocating with non-trivial constructor on the pageable host memory is not supported");
+      void *mem = std::aligned_alloc(host::impl::default_alignment, host::impl::pad(n * sizeof(element_type)));
+      return typename host::impl::make_host_unique_selector<T>::unbounded_array{reinterpret_cast<element_type *>(mem),
+                                                                                host::impl::Deleter{false}};
+    }
+
+    // No check for the trivial constructor, make it clear in the interface
+    template <typename T>
+    typename host::impl::make_host_unique_selector<T>::non_array make_host_unique_uninitialized() {
+      void *mem = std::aligned_alloc(host::impl::default_alignment, host::impl::pad(sizeof(T)));
+      return typename host::impl::make_host_unique_selector<T>::non_array{reinterpret_cast<T *>(mem),
+                                                                          host::impl::Deleter{false}};
+    }
+
+    template <typename T>
+    typename host::impl::make_host_unique_selector<T>::unbounded_array make_host_unique_uninitialized(size_t n) {
+      using element_type = typename std::remove_extent<T>::type;
+      void *mem = std::aligned_alloc(host::impl::default_alignment, host::impl::pad(n * sizeof(element_type)));
+      return typename host::impl::make_host_unique_selector<T>::unbounded_array{reinterpret_cast<element_type *>(mem),
+                                                                                host::impl::Deleter{false}};
+    }
+
     // Allocate pinned host memory
     template <typename T>
     typename host::impl::make_host_unique_selector<T>::non_array make_host_unique(cudaStream_t stream) {
       static_assert(std::is_trivially_constructible<T>::value,
                     "Allocating with non-trivial constructor on the pinned host memory is not supported");
       void *mem = allocate_host(sizeof(T), stream);
-      return typename host::impl::make_host_unique_selector<T>::non_array{reinterpret_cast<T *>(mem)};
+      return typename host::impl::make_host_unique_selector<T>::non_array{reinterpret_cast<T *>(mem),
+                                                                          host::impl::Deleter{true}};
     }
 
     template <typename T>
@@ -51,17 +107,16 @@ namespace cms {
       static_assert(std::is_trivially_constructible<element_type>::value,
                     "Allocating with non-trivial constructor on the pinned host memory is not supported");
       void *mem = allocate_host(n * sizeof(element_type), stream);
-      return typename host::impl::make_host_unique_selector<T>::unbounded_array{reinterpret_cast<element_type *>(mem)};
+      return typename host::impl::make_host_unique_selector<T>::unbounded_array{reinterpret_cast<element_type *>(mem),
+                                                                                host::impl::Deleter{true}};
     }
-
-    template <typename T, typename... Args>
-    typename host::impl::make_host_unique_selector<T>::bounded_array make_host_unique(Args &&...) = delete;
 
     // No check for the trivial constructor, make it clear in the interface
     template <typename T>
     typename host::impl::make_host_unique_selector<T>::non_array make_host_unique_uninitialized(cudaStream_t stream) {
       void *mem = allocate_host(sizeof(T), stream);
-      return typename host::impl::make_host_unique_selector<T>::non_array{reinterpret_cast<T *>(mem)};
+      return typename host::impl::make_host_unique_selector<T>::non_array{reinterpret_cast<T *>(mem),
+                                                                          host::impl::Deleter{true}};
     }
 
     template <typename T>
@@ -69,11 +124,17 @@ namespace cms {
         size_t n, cudaStream_t stream) {
       using element_type = typename std::remove_extent<T>::type;
       void *mem = allocate_host(n * sizeof(element_type), stream);
-      return typename host::impl::make_host_unique_selector<T>::unbounded_array{reinterpret_cast<element_type *>(mem)};
+      return typename host::impl::make_host_unique_selector<T>::unbounded_array{reinterpret_cast<element_type *>(mem),
+                                                                                host::impl::Deleter{true}};
     }
+
+    // Forbid arrays of known bounds (to match std::unique_ptr)
+    template <typename T, typename... Args>
+    typename host::impl::make_host_unique_selector<T>::bounded_array make_host_unique(Args &&...) = delete;
 
     template <typename T, typename... Args>
     typename host::impl::make_host_unique_selector<T>::bounded_array make_host_unique_uninitialized(Args &&...) = delete;
+
   }  // namespace cuda
 }  // namespace cms
 
